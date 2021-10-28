e7d45017c831f61054c5e6654d2210ddd055ab53
Lukas Jung <lukas.jung@online.ch>
2021-10-28 23:00:21 +0200

    Added WebChat Control

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import ReactWebChat, { createDirectLine } from 'botframework-webchat';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+const storedUserName = sessionStorage.getItem("storedUserName");
 
-export default App;
+const botSecret = "cXeVg9qGnZM.KPeyDLf7nl26Kg1SY1UFCTryjCQTQr6i9Qie_2vcuq8";
+export default () => {
+  const [userId, setUserId] = useState(storedUserName);
+  const [webChatActive, setWebChatActive] = useState(storedUserName && storedUserName.length > 0);
+  const directLine = useMemo(() => createDirectLine({ secret: botSecret }), []);
+
+  sessionStorage.setItem("storedUserName", userId);
+  return <>
+    <div className="userIdArea">
+      User Name:
+      <input disabled={webChatActive} type="text" value={userId} onChange={e => setUserId(e.target.value)} ></input>
+      { !webChatActive && <input type="button" value="Go!" onClick={() => setWebChatActive(userId && userId.length > 0)} /> }
+    </div>
+    {webChatActive && <ReactWebChat className="webChat" locale="de-DE" directLine={directLine} userID={userId} />}
+  </>;
+};
