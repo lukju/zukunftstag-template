35002e6db73e11043437263eb04e2f12160ec225
Lukas Jung <lukas.jung@online.ch>
2021-11-04 13:40:16 +0100

    -

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,17 @@
-function getQueryParam(name: any){
-    name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(window.location.search)
-    return name ? decodeURIComponent(name[1]): undefined;
- }
+function getQueryParam(name: any) {
+    name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(window.location.search)
+    return name ? decodeURIComponent(name[1]) : undefined;
+}
+
+let botSecret = getQueryParam("secret");
+if (botSecret) {
+    sessionStorage.setItem("botSecret", botSecret);
+    location.reload();
+} else {
+    botSecret = sessionStorage.getItem("botSecret") || '';
+}
 
 export const env = {
     owner: getQueryParam("owner") || '<unbekannt>',
-    botSecret: getQueryParam("secret")
+    botSecret: botSecret
 };
